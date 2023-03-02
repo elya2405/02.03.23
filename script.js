da97abaee5fef4f936c8549b559c405d6361d4dc
kanybekova eleonora <eelya250@gmail.com>
2023-03-02 14:47:01 +0600

    output a list from an array and an object

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const exampleList = ["apples", "pears", "oranges"];
+const exampleList1 = ["apples", "pears", "oranges"];
 const ul = document.createElement("ul");
 document.body.append(ul);
-for (i = 0; i < exampleList.length; i++) {
-  const li = document.createElement("li");
-  ul.append(li);
-  li.textContent = exampleList[i];
+for (i = 0; i < exampleList1.length; i++) {
+  const list = document.createElement("li");
+  ul.append(list);
+  list.textContent = exampleList1[i];
+}
+
+const exampleList2 = [
+  { name: "bakyt", age: 18 },
+  { name: "dosbol", age: 32 },
+];
+const ul1 = document.createElement("ul");
+document.body.append(ul1);
+for (i = 0; i < exampleList2.length; i++) {
+  const list1 = document.createElement("li");
+  ul1.append(list1);
+  list1.textContent = exampleList2[i].name + " - " + exampleList2[i].age;
 }
